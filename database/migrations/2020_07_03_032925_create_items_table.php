961c90e93a6839ceb3a04ea1b26887070a5fb640
Andhi Pramudya Wiguna <andipw.matoa@gmail.com>
2020-07-04 03:20:56 +0700

    first commit

---
+++ database/migrations/2020_07_03_032925_create_items_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use phpDocumentor\Reflection\Types\Nullable;
+
+class CreateItemsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('items', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->string('description')->nullable();
+            $table->integer('stock')->nullable();
+            $table->integer('price')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('items');
+    }
+}
---
+++ resources/views/item/index.blade.php
@@ -0,0 +1,9 @@
+@extends('adminlte.master')
+
+@section('content')
+    <div class="ml-3 mt-3">
+        <h1>Baik Lah Jalan Juga</h1>
+    </div>
+
+
+@endsection
